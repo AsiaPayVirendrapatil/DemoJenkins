392128048fdc41c40d1912317a2bc1e2a05c5703
Virendra patil <virendrapatil@Virendras-MacBook-Pro.local>
2020-03-12 15:58:18 +0530

    eight commit

---
+++ OTPdemo/DoneView.swift
@@ -0,0 +1,28 @@
+//
+//  DoneView.swift
+//  OTPdemo
+//
+//  Created by Virendra patil on 12/03/20.
+//  Copyright © 2020 Virendra patil. All rights reserved.
+//
+
+import UIKit
+
+class DoneView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
