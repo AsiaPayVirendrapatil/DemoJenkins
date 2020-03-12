40ef969f91b07943e04823695e13d682b1e1418b
Virendra patil <virendrapatil@Virendras-MacBook-Pro.local>
2020-03-12 12:44:01 +0530

    third commit

--- OTPdemo/ViewController.swift
+++ OTPdemo/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("Virendra Patil Done notifly tester")
+        
         let favoritesVC = CardView()
         favoritesVC.title = "Favorites"
         favoritesVC.view.backgroundColor = UIColor.orange
